95012400253b2a743f1309da0de814d6f3c2f270
SamuelHenr <samuelhma91@gmail.com>
2020-12-08 11:45:37 -0300

    maximize imagens

--- js/scripts.js
+++ js/scripts.js
@@ -54,6 +54,26 @@
 })(jQuery) // End of use strict
 
 // Insere imagens das crianças
+function maximize_images(index) {
+  // Get the modal
+  var modal = document.getElementById('myModal')
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById('foto' + index)
+  var modalImg = document.getElementById('img01')
+  var captionText = document.getElementById('caption')
+  modal.style.display = 'block'
+  modalImg.src = img.src
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName('close')[0]
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = 'none'
+  }
+}
+
 function insert_images() {
   var inner = ''
   var images = document.getElementById('fotos')
@@ -63,7 +83,11 @@ function insert_images() {
 
     inner +=
       '<div class="col-lg-4 col-md-6 col-sm-12">' +
-      '<img class="img-thumbnail" style="width: 400px; height: 300px;" src="./assets/img/Criancas/' +
+      '<img id="foto' +
+      i +
+      '" class="img-thumbnail myImg" onclick="maximize_images(' +
+      i +
+      ')" style="width: 400px; height: 300px;" src="./assets/img/Criancas/' +
       i +
       '.jpg"/>' +
       '</div>'
