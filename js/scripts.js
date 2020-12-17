4e968bb8d1ec57dc13122b2eb27cbb5761161c4c
SamuelHenr <samuelhma91@gmail.com>
2020-12-16 23:12:36 -0300

    fotos

--- js/scripts.js
+++ js/scripts.js
@@ -66,11 +66,12 @@ function maximize_images(index) {
   modalImg.src = img.src
 
   // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName('close')[0]
+  var span = document.getElementById('fechar')
 
   // When the user clicks on <span> (x), close the modal
   span.onclick = function () {
     modal.style.display = 'none'
+    console.log('yo')
   }
 }
 
